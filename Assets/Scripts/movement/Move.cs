562c7af839791b92fcde05a3aa6998a89af51f73
SNORC <fasttimirobot@gmail.com>
2025-07-25 01:11:56 +0400

    добавлен поворот персонажа при ходьбе

--- Assets/Scripts/movement/Move.cs
+++ Assets/Scripts/movement/Move.cs
@@ -10,11 +10,24 @@ public class Move : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    void Update()
+    {
+        Flip();
+    }
 
     void FixedUpdate()
     {
+       
+
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+    void Flip()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        if(Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
     }
 }
 

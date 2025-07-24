0c684e2fa0f9fd93d83adfa1cd91afaf971a851b
nastyyyam <anastasiamoiseeva506@gmail.com>
2025-07-24 17:34:06 +0300

    Система передвижения персонажа(тестовая)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); // A/D или стрелки влево/вправо
+        movement.y = Input.GetAxisRaw("Vertical");   // W/S или стрелки вверх/вниз
+        movement = movement.normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}

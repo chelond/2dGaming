b9e3aaa9d8b17b454ed47447da057db969532d35
nastyyyam <anastasiamoiseeva506@gmail.com>
2025-07-24 18:01:23 +0300

    Система передвижения персонажа(тестовая)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private bool isGrounded;
+    private float moveInput;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal"); // A/D или стрелки влево/вправо
-        movement.y = Input.GetAxisRaw("Vertical");   // W/S или стрелки вверх/вниз
-        movement = movement.normalized;
+        moveInput = Input.GetAxisRaw("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        // Булевый параметр: true если персонаж двигается по горизонтали
+        if (animator != null)
+            animator.SetBool("isRunning", Mathf.Abs(moveInput) > 0.01f);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
     }
 }
